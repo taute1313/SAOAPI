8816b259140368355e6096dc4367a8e35b09a4d0
taute1313 <92098185+taute1313@users.noreply.github.com>
2025-12-12 14:12:24 +0100

    Update router.py

--- fastapi-aossample-codespaces/app/api/v1/router.py
+++ fastapi-aossample-codespaces/app/api/v1/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from .endpoints import tasks
+from .endpoints import tasks, auth
 
 api_router = APIRouter(prefix="/api/v1")
 api_router.include_router(tasks.router)
+api_router.include_router(auth.router)
